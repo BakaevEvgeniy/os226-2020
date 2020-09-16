db431172177d833d4e092fce6de688e472e469a9
BakaevEvgeniy <evgeniybakaev@bk.ru>
2020-09-16 16:27:20 +0300

    homework 1

--- main.c
+++ main.c
@@ -1,19 +1,58 @@
-
 #include <stdbool.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-int echo(int argc, char *argv[]) {
-	for (int i = 1; i < argc; ++i) {
+int retCode = 0;
+
+int echo(int argc, char* argv[])
+{
+	for (int i = 1; i < argc; ++i)
+	{
 		printf("%s%c", argv[i], i == argc - 1 ? '\n' : ' ');
 	}
 	return argc - 1;
 }
 
-int retcode(int argc, char *argv[]) {
+int retcode(int argc, char* argv[])
+{
+	printf("%d\n", retCode);
+	return 0;
 }
 
-int main(int argc, char *argv[]) {
+void parse(int argc, char* argv[], char* input)
+{
+	char* nextCommand;
+	char* nextWord;
+	char* parsedInput = strtok_s(input, ";\n", &nextCommand);
+	while (parsedInput != NULL)
+	{
+		char* words = strtok_s(parsedInput, " ", &nextWord);
+		argc = 0;
+		while (words != NULL)
+		{
+			argv[argc] = words;
+			argc++;
+			words = strtok_s(NULL, " ", &nextWord);
+		}
+
+		if (strcmp(argv[0], "echo") == 0)
+			retCode = echo(argc, argv);
+		else if (strcmp(argv[0], "retcode") == 0)
+			retcode(argc, argv);
+		else
+			printf("Unknown command.\n");
+
+		parsedInput = strtok_s(NULL, ";\n", &nextCommand);
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	char* input[256];
+	
+	while (fgets(input, 256, stdin) != NULL)
+		parse(argc, argv, input);
+	
 	return 0;
 }
